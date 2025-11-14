b61d858d371930c450fb3b011fcdf40bc6626b06
bennyboy15 <benjaminharvey2000@gmail.com>
2025-11-14 13:07:23 +1030

    cron job added

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,11 +4,13 @@ import authRoutes from "./routes/auth.routes.js";
 import bookRoutes from "./routes/book.routes.js";
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
+import job from "./lib/cron.js";
 
 config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+job.start(); // CRON JOB
 app.use(express.json());
 app.use(cors());
 
--- mobile/store/authStore.js
+++ mobile/store/authStore.js
@@ -1,20 +1,21 @@
-import {create} from "zustand";
+import { create } from "zustand";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const useAuthStore = create((set,get) => ({
+export const useAuthStore = create((set, get) => ({
     user: null,
     token: null,
     isLoading: false,
+    isCheckingAuth: false,
 
     register: async (name, username, email, password) => {
         try {
-            set({isLoading: true});
+            set({ isLoading: true });
             const response = await fetch("https://bookbook-8jgx.onrender.com/api/auth/register", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
                 },
-                body: JSON.stringify({name, username,email,password})
+                body: JSON.stringify({ name, username, email, password })
             });
             const data = await response.json();
 
@@ -23,12 +24,60 @@ export const useAuthStore = create((set,get) => ({
             await AsyncStorage.setItem("user", JSON.stringify(data.user));
             await AsyncStorage.setItem("token", data.token);
 
-            set({token: data.token, user: data.user, isLoading: false});
-            return {success:true}
+            set({ token: data.token, user: data.user, isLoading: false });
+            return { success: true }
 
         } catch (error) {
-            set({isLoading: false});
-            return {success:false, error:error.message}
+            set({ isLoading: false });
+            return { success: false, error: error.message }
         }
-    }
+    },
+
+    login: async (username, password) => {
+        try {
+            set({ isLoading: true });
+            const response = await fetch("https://bookbook-8jgx.onrender.com/api/auth/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ username, password })
+            });
+            const data = await response.json();
+
+            if (!response.ok) throw new Error(data.message || "Something went wrong");
+
+            await AsyncStorage.setItem("user", JSON.stringify(data.user));
+            await AsyncStorage.setItem("token", data.token);
+
+            set({ token: data.token, user: data.user, isLoading: false });
+            return { success: true }
+
+        } catch (error) {
+            set({ isLoading: false });
+            return { success: false, error: error.message }
+        }
+    },
+
+    logout: async () => {
+        await AsyncStorage.removeItem("token");
+        await AsyncStorage.removeItem("user");
+        set({ token: null, user: null });
+    },
+
+    checkAuth: async () => {
+        try {
+            set({ isCheckingAuth: true });
+            const token = await AsyncStorage.getItem("token");
+            const userJson = await AsyncStorage.getItem("user");
+            const user = userJson ? JSON.parse(userJson) : null;
+
+            set({ token, user });
+        } catch (error) {
+            console.log("Auth check failed", error);
+        } finally {
+            set({ isCheckingAuth: false });
+        }
+    },
+
 }))
