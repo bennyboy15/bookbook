0187d96f34588443466f838ea48c7f4297fed75b
bennyboy15 <benjaminharvey2000@gmail.com>
2025-11-14 11:12:33 +1030

    Login UI

--- mobile/app/(auth)/index.jsx
+++ mobile/app/(auth)/index.jsx
@@ -1,11 +1,108 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ActivityIndicator, KeyboardAvoidingView, Platform, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import styles from "../../assets/styles/login.styles.js";
+import { useState } from 'react';
+import { Image } from "expo-image";
+import { Ionicons } from "@expo/vector-icons";
+import COLORS from '../../constants/colors.js';
+import { Link } from "expo-router"
 
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+
+
   return (
-    <View>
-      <Text>Login Screen</Text>
-    </View>
+    <KeyboardAvoidingView style={{flex: 1}} behavior={Platform.OS === "ios" ? "padding" : "height"}>
+      <View style={styles.container}>
+
+        <View style={styles.topIllustration}>
+          <Image source={require("../../assets/images/reading-glasses.svg")} style={styles.illustrationImage} contentFit='contain' />
+        </View>
+
+        <View style={styles.card}>
+          <View style={styles.formContainer}>
+
+            {/* USERNAME */}
+            <View style={styles.inputGroup}>
+              <Text style={styles.label}>Username</Text>
+              <View style={styles.inputContainer}>
+                <Ionicons
+                  name="person-outline"
+                  size={20}
+                  color={COLORS.primary}
+                  style={styles.inputIcon}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Enter your username"
+                  placeholderTextColor={COLORS.placeholderText}
+                  value={username}
+                  onChangeText={setUsername}
+                  autoCapitalize="none"
+                />
+              </View>
+            </View>
+
+            {/* PASSWORD */}
+            <View style={styles.inputGroup}>
+              <Text style={styles.label}>Password</Text>
+              <View style={styles.inputContainer}>
+                {/* LEFT ICON */}
+                <Ionicons
+                  name="lock-closed-outline"
+                  size={20}
+                  color={COLORS.primary}
+                  style={styles.inputIcon}
+                />
+                {/* INPUT */}
+                <TextInput
+                  style={styles.input}
+                  placeholder="Enter your password"
+                  placeholderTextColor={COLORS.placeholderText}
+                  value={password}
+                  onChangeText={setPassword}
+                  secureTextEntry={!showPassword}
+                />
+
+                <TouchableOpacity
+                  onPress={() => setShowPassword(!showPassword)}
+                  style={styles.eyeIcon}
+                >
+                  <Ionicons
+                    name={showPassword ? "eye-outline" : "eye-off-outline"}
+                    size={20}
+                    color={COLORS.primary}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+
+            {/* LOGIN BTN */}
+            <TouchableOpacity style={styles.button} onPress={() => console.log("LOGIN")} disabled={isLoading}>
+              {isLoading ? (
+                <ActivityIndicator color="#fff" />
+              ) : (
+                <Text style={styles.buttonText}>Login</Text>
+              )}
+            </TouchableOpacity>
+
+            {/* FOOTER */}
+            <View style={styles.footer}>
+              <Text style={styles.footerText}>Don&apos;t have an account?</Text>
+              <Link href="/signup" asChild>
+                <TouchableOpacity>
+                  <Text style={styles.link}>Sign Up</Text>
+                </TouchableOpacity>
+              </Link>
+            </View>
+
+          </View>
+
+        </View>
+      </View>
+    </KeyboardAvoidingView>
   )
 }
-
-const styles = StyleSheet.create({})
